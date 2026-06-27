ce796064d581915491c3d379003f89df0a96d7f8
Karans03 <155185692+Karans03@users.noreply.github.com>
2026-06-27 00:05:10 -0500

    Debugging ah

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,9 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'../Frontend')));
+app.use(express.static(path.join(__dirname,'Frontend')));
+
+console.log(path.join(__dirname,'Frontend'));
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
